11afd10695c379a6386ce5de059cc4094f2b1a53
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-22 10:32:03 +0200

    make it better

--- config/config.js
+++ config/config.js
@@ -1,22 +1,20 @@
 require("dotenv").config();
-module.exports={
-  "development": {
-    "url":process.env.DATABASE_URL_DEV,
-    "dialect": "postgres",
-    "operatorsAliases":"0"
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL_DEV,
+    dialect: "postgres",
+    operatorsAliases: "0",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    url: process.env.DATABASE_URL_DEV,
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+};
